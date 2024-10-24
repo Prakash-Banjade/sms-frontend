e59bd76946a6ee98cca8479e3cba6ab1c6109a7d
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-24 12:50:46 +0545

    remove unused

--- src/apps/student/student-routes.tsx
+++ src/apps/student/student-routes.tsx
@@ -1,15 +1,12 @@
 import AppRootLayout from '@/components/app-sidebar-layout/root-layout';
 import RequireAuth from '@/components/auth/require-auth';
-
 import { Role } from '@/types/global.type';
 import { Route, Routes } from 'react-router-dom';
 import { studentSidebarMenuItems } from './layout/sidebar-items';
 import AssignmentPage from './pages/academics/assigment.page';
 import HomeWorkPage from './pages/academics/homework.page';
-import ClassRoutinePage from './pages/academics/class-routine.page';
 import StudentAttendenceListPage from './pages/academics/attendence.page';
-
-
+import ClassRoutineListPage from '../admin/pages/class-routine/class-routine-list.page';
 
 const StudentRoutes = () => {
   return (
@@ -20,7 +17,7 @@ const StudentRoutes = () => {
           <Route path="assignments" element={<AssignmentPage />} />
           <Route path="homework" element={<HomeWorkPage />} />
           <Route path="attendance" element={<StudentAttendenceListPage />} />
-          <Route path="class-routine" element={<ClassRoutinePage />} />
+          <Route path="class-routine" element={<ClassRoutineListPage />} />
         </Route>
       </Route>
     </Routes>
