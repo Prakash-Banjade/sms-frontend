fd86be33c796db9a1a95b93e8d5936d081d9b6ac
Prakash Banjade <prakashbanjade191@gmail.com>
2024-10-24 17:05:33 +0545

    change app name

--- src/components/app-sidebar-layout/sidebar-header.tsx
+++ src/components/app-sidebar-layout/sidebar-header.tsx
@@ -14,8 +14,8 @@ export function AppSidebarHeader() {
                             <School className="size-5" />
                         </div>
                         <div className="flex flex-col gap-0.5 leading-none">
-                            <span className="font-semibold">Apex Higher</span>
-                            <span className="">Secondary School</span>
+                            <span className="font-semibold">Abhyam Academy</span>
+                            <span className="text-xs mt-1">Comming Soon...</span>
                         </div>
                     </SidebarMenuButton>
                 </SidebarMenuItem>
