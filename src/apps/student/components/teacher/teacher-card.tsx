8b0b0b2c72d236f8f61ccf91986ba6ea734fa39b
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2024-11-10 15:27:09 +0545

    teacher card done

--- src/apps/student/components/teacher/teacher-card.tsx
+++ src/apps/student/components/teacher/teacher-card.tsx
@@ -1,35 +1,31 @@
 import { ProfileAvatar } from '@/components/ui/avatar'
-import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { getImageUrl } from '@/lib/utils'
-import { Teacher } from '@/types/teacher.type'
-
-
-import { Mail, Phone } from 'lucide-react'
+import { St_Teacher } from '@/types/teacher.type'
+import { Book, Mail, Phone } from 'lucide-react'
 
 type Props = {
-    teachers: Teacher[]
+    teachers: St_Teacher[]
 }
 
 const TeacherList = ({ teachers }: Props) => {
     return (
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {teachers?.map((teacher) => (
-                <Card key={teacher.id} className="flex flex-col h-full">
+            {teachers?.map((teacher, ind) => (
+                <Card key={ind} className="flex flex-col h-full">
                     <CardHeader className="flex-grow">
                         <div className="flex items-center space-x-4">
-                            <ProfileAvatar name={teacher.teacherFullName} src={getImageUrl(teacher.profileImage?.url, 'w=40')} className="size-10" />
-                            <div>
-                                <CardTitle className="text-lg">{teacher.teacherFullName}</CardTitle>
-                                <Badge variant="info" className="mt-1 capitalize">
-                                    {/* {teacher.} */}
-                                    math
-                                </Badge>
-                            </div>
+                            <ProfileAvatar name={teacher.teacherFullName} src={getImageUrl(teacher.profileImageUrl, 'w=40')} className="size-10" />
+                            <CardTitle className="text-lg">{teacher.teacherFullName}</CardTitle>
+
                         </div>
                     </CardHeader>
                     <CardContent className='flex flex-col gap-3'>
+                        <div className='flex gap-2 items-center text-muted-foreground'>
+                            <Book className=" h-4 w-4" />
+                            {teacher.subject}
+                        </div>
                         <a
                             href={`mailto:${teacher.email}`}
                             className="text-muted-foreground hover:underline hover:text-blue-500 flex items-center"
