75ab72a9406b7a7fe81a02a4d75a07c708293717
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2024-11-14 14:53:59 +0545

    changed select and check if no data found

--- src/apps/student/pages/student-library-details.page.tsx
+++ src/apps/student/pages/student-library-details.page.tsx
@@ -2,45 +2,80 @@ import { useGetBookTransactions } from "@/apps/admin/components/library/actions"
 import LibraryStatistics from "../components/library/library-stat"
 import LibraryCard from "../components/library/library-card";
 import { createQueryString } from "@/utils/create-query-string";
-import { useSearchParams } from "react-router-dom";
-import { FacetedFilter } from "@/components/data-table/faceted-filter";
 import { EBookTransactionStatus } from "@/types/global.type";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useCustomSearchParams } from "@/hooks/useCustomSearchParams";
+import { useState } from "react";
 
 
 const StudentLibraryDetailsPage = () => {
-    const [searchParams] = useSearchParams()
+    const { searchParams, setSearchParams } = useCustomSearchParams()
+    const [searchFilters, setSearchFilters] = useState({
+        status: searchParams.get('status') ?? '',
+    })
     const { data, isLoading } = useGetBookTransactions(
         {
             queryString: createQueryString({
-                search: searchParams.get('status'),
+                status: searchParams.get('status'),
             })
         }
     );
 
-    if (isLoading) return <div>Loading...</div>;
-    if (!data || data?.data.length === 0) {
-        return (
-            <div className="h-[50vh] flex items-center justify-center font-semibold text-muted-foreground">
-                No library record available!!
-            </div>
-        );
+    const handleSearchFiltersChange = (value: string, key: keyof typeof searchFilters) => {
+        setSearchFilters({ ...searchFilters, [key]: value })
+        setSearchParams(key, value)
     }
+
     return (
         <div className="mx-auto container flex flex-col gap-6">
 
             <h2 className="text-lg font-semibold">My Library</h2>
             <LibraryStatistics />
-            <section className="flex flex-wrap  justify-between ">
+            <section className="flex flex-wrap  justify-between  ">
                 <h2 className="text-lg font-semibold">Book History</h2>
-                <FacetedFilter title="Trasaction Status" searchKey="status" options={Object.entries(EBookTransactionStatus).map(([_, value]) => ({ label: value, value }))} />
+
+                <Select onValueChange={val => handleSearchFiltersChange(val, 'status')}
+                    value={searchFilters.status}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder={"Select Status"} />
+                    </SelectTrigger>
+                    <SelectContent >
+                        <SelectGroup>
+                            <SelectLabel>Status</SelectLabel>
+                            {
+                                Object.entries(EBookTransactionStatus).map(([key, value]) => (
+                                    <SelectItem key={key} value={value}>
+                                        {key}
+                                    </SelectItem>
+                                ))
+                            }
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
             </section>
-            <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  items-center gap-10">
-                {
-                    data?.data.map((transaction) => (
-                        <LibraryCard transaction={transaction} />
-                    ))
-                }
-            </div>
+
+
+            {
+                (!data || data?.data.length === 0) ?
+                    <div className="h-[50vh] flex items-center justify-center  mx-auto font-semibold text-center text-muted-foreground">
+                        No library record available!!
+                    </div>
+                    :
+                    (
+                        isLoading ? <Skeleton className="max-w-4xl h-1/2" /> :
+                            (
+                                <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  items-center gap-10">
+                                    {data?.data.map((transaction) => (
+                                        <LibraryCard transaction={transaction} />
+                                    ))}
+                                </div>
+                            )
+                    )
+
+            }
+
         </div>
     )
 }
