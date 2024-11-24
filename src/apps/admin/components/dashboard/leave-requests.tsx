ac2f7f07a86d7a7e94ab0e105629313b8fc3396b
Prakash Banjade <prakashbanjade191@gmail.com>
2024-11-24 07:57:10 +0545

    fix leave request days

--- src/apps/admin/components/dashboard/leave-requests.tsx
+++ src/apps/admin/components/dashboard/leave-requests.tsx
@@ -4,7 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CalendarOff, Clock } from 'lucide-react'
 import { useGetAdminDashboardLeaveRequests } from "../../data-access/dashboard-data-access"
 import { ProfileAvatar } from "@/components/ui/avatar"
-import { formatDistanceToNow } from "date-fns"
+import { differenceInDays, format, formatDistanceToNow, isSameMonth } from "date-fns"
 import { getImageUrl } from "@/lib/utils"
 import { Link } from "react-router-dom"
 import { useAppMutation } from "@/hooks/useAppMutation"
@@ -44,29 +44,41 @@ export default function Dashboard_LeaveRequests() {
                             !!leaveRequests?.studentsLeaveRequests?.data?.length ?
                                 <div className="space-y-4">
                                     {
-                                        leaveRequests?.studentsLeaveRequests?.data?.map(leaveRequest => (
-                                            <div className="flex gap-4 rounded-lg p-4 bg-secondary/20" key={leaveRequest.id}>
-                                                <ProfileAvatar className="size-12" name={leaveRequest.studentName} src={getImageUrl(leaveRequest.profileImageUrl, 'w=48')} />
-                                                <div className="flex-1 space-y-1">
-                                                    <div className="flex items-center gap-1 flex-wrap">
-                                                        <p className="font-medium">{leaveRequest.studentName}</p>
-                                                        <span className="text-sm text-muted-foreground">{leaveRequest.classRoomName}</span>
+                                        leaveRequests?.studentsLeaveRequests?.data?.map(leaveRequest => {
+                                            const difference = differenceInDays(new Date(leaveRequest.leaveTo), new Date(leaveRequest.leaveFrom));
+                                            const leaveFromDate = format(new Date(leaveRequest.leaveFrom), 'MMM dd');
+                                            const leaveToDate = format(new Date(leaveRequest.leaveTo), 'MMM dd');
+
+                                            const leaveDaysString = difference > 0
+                                                ? isSameMonth(leaveRequest.leaveTo, leaveRequest.leaveFrom)
+                                                    ? `${difference + 1} days (${leaveFromDate}-${format(leaveRequest.leaveTo, 'dd')})`
+                                                    : `${difference + 1} days (${leaveFromDate} - ${leaveToDate})`
+                                                : `${difference + 1} day (${leaveFromDate})`;
+
+                                            return (
+                                                <div className="flex gap-4 rounded-lg p-4 bg-secondary/20" key={leaveRequest.id}>
+                                                    <ProfileAvatar className="size-12" name={leaveRequest.studentName} src={getImageUrl(leaveRequest.profileImageUrl, 'w=48')} />
+                                                    <div className="flex-1 space-y-1">
+                                                        <div className="flex items-center gap-1 flex-wrap">
+                                                            <p className="font-medium">{leaveRequest.studentName}</p>
+                                                            <span className="text-sm text-muted-foreground">{leaveRequest.classRoomName}</span>
+                                                        </div>
+                                                        <div className="flex items-center gap-2">
+                                                            <CalendarOff className="size-4 text-muted-foreground" />
+                                                            <p className="text-sm text-muted-foreground">{leaveDaysString}</p>
+                                                        </div>
+                                                        <div className="flex items-center gap-2">
+                                                            <Clock className="size-4 text-muted-foreground" />
+                                                            <p className="text-sm text-muted-foreground">Requested {formatDistanceToNow(leaveRequest.createdAt)} ago</p>
+                                                        </div>
                                                     </div>
-                                                    <div className="flex items-center gap-2">
-                                                        <CalendarOff className="size-4 text-muted-foreground" />
-                                                        <p className="text-sm text-muted-foreground">3 days (Dec 15-17)</p>
-                                                    </div>
-                                                    <div className="flex items-center gap-2">
-                                                        <Clock className="size-4 text-muted-foreground" />
-                                                        <p className="text-sm text-muted-foreground">Requested {formatDistanceToNow(new Date(leaveRequest.createdAt))} ago</p>
+                                                    <div className="flex gap-1 flex-col">
+                                                        <Button type="button" variant={'ghost'} size={'sm'} onClick={() => handleUpdateStatus(leaveRequest.id, ELeaveRequestStatus.REJECTED)}>Reject</Button>
+                                                        <Button type="button" size={'sm'} onClick={() => handleUpdateStatus(leaveRequest.id, ELeaveRequestStatus.APPROVED)}>Approve</Button>
                                                     </div>
                                                 </div>
-                                                <div className="flex gap-1 flex-col">
-                                                    <Button type="button" variant={'ghost'} size={'sm'} onClick={() => handleUpdateStatus(leaveRequest.id, ELeaveRequestStatus.REJECTED)}>Reject</Button>
-                                                    <Button type="button" size={'sm'} onClick={() => handleUpdateStatus(leaveRequest.id, ELeaveRequestStatus.APPROVED)}>Approve</Button>
-                                                </div>
-                                            </div>
-                                        ))
+                                            )
+                                        })
                                     }
                                     <div className="flex justify-between gap-5 items-center text-sm text-muted-foreground">
                                         <span>Total: {leaveRequests?.studentsLeaveRequests?.total}</span>
@@ -93,11 +105,11 @@ export default function Dashboard_LeaveRequests() {
                                                     <p className="font-medium">{leaveRequest.teacherName}</p>
                                                     <div className="flex items-center gap-2">
                                                         <CalendarOff className="size-4 text-muted-foreground" />
-                                                        <p className="text-sm text-muted-foreground">5 days (Dec 18-22)</p>
+                                                        <p className="text-sm text-muted-foreground">{differenceInDays(leaveRequest.leaveTo, leaveRequest.leaveFrom)} days (Dec 18-22)</p>
                                                     </div>
                                                     <div className="flex items-center gap-2">
                                                         <Clock className="size-4 text-muted-foreground" />
-                                                        <p className="text-sm text-muted-foreground">Requested {formatDistanceToNow(new Date(leaveRequest.createdAt))} ago</p>
+                                                        <p className="text-sm text-muted-foreground">Requested {formatDistanceToNow(leaveRequest.createdAt)} ago</p>
                                                     </div>
                                                 </div>
                                                 <div className="flex gap-1 flex-col">
