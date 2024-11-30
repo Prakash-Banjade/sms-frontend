2a98fafa9ae2a73c814092f8380bc174a111b07a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-11-30 21:42:24 +0545

    add currency

--- src/apps/admin/components/settings/general-settings-form-fields.tsx
+++ src/apps/admin/components/settings/general-settings-form-fields.tsx
@@ -6,6 +6,19 @@ export default function GeneralSettingsFormFields() {
         <section className="@container">
             <section className="grid @xl:grid-cols-2 gap-12 grid-cols-1">
                 <section className="space-y-4">
+                    <h3 className="uppercase font-bold text-xs text-muted-foreground">System settings</h3>
+                    <div className="flex flex-col gap-4">
+                        <AppForm.Text<TGeneralSettingsFormType>
+                            name="currency"
+                            label="Currency"
+                            placeholder={`e.g. NPR`}
+                            description="Currency for the system"
+                            required
+                        />
+                    </div>
+                </section>
+
+                <section className="space-y-4">
                     <h3 className="uppercase font-bold text-xs text-muted-foreground">library settings</h3>
                     <div className="flex flex-col gap-4">
                         <AppForm.Number<TGeneralSettingsFormType>
@@ -18,9 +31,6 @@ export default function GeneralSettingsFormFields() {
                     </div>
                 </section>
 
-                {/* <section className="space-y-4">
-                    <h3 className="uppercase font-bold text-xs text-muted-foreground">some other settings</h3>
-                </section> */}
             </section>
         </section>
     )
--- src/apps/admin/schemas/general-settings.schema.ts
+++ src/apps/admin/schemas/general-settings.schema.ts
@@ -2,10 +2,12 @@ import { z } from "zod"
 
 export const generalSettingsFormSchema = z.object({
     libraryFine: z.coerce.number().nonnegative({ message: 'Library fine must be a non-negative number' }).optional(),
+    currency: z.string().optional(),
 })
 
 export type TGeneralSettingsFormType = z.infer<typeof generalSettingsFormSchema>;
 
 export const generalSettingsDefaultValues: Partial<TGeneralSettingsFormType> = {
     libraryFine: 0,
+    currency: '',
 }
