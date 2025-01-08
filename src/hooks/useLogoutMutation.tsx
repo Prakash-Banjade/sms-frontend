77f24fb7d1a2c3e2fb0e16234652c577fe1c8490
Prakash Banjade <prakashbanjade191@gmail.com>
2025-01-08 15:31:42 +0545

    feat: clear cache and remove persist on logout

--- src/hooks/useLogoutMutation.tsx
+++ src/hooks/useLogoutMutation.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from "@/contexts/auth-provider";
 import { useLocation, useNavigate } from "react-router-dom";
 import { QueryKey } from "@/react-query/queryKeys";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAxios } from "@/services/api";
 import { CookieKey } from "@/CONSTANTS";
 import { deleteCookie } from "@/utils/cookie";
@@ -11,14 +11,17 @@ export function useLogoutMutation() {
     const navigate = useNavigate();
     const location = useLocation();
     const axios = useAxios();
+    const queryClient = useQueryClient();
 
     const { mutateAsync, isPending } = useMutation<any, any>({
         mutationFn: async () => {
             await axios.post(`${import.meta.env.VITE_API_URL}/${QueryKey.AUTH_LOGOUT}`);
         },
         onSuccess: () => {
+            localStorage.removeItem("persist");
             Object.values(CookieKey).forEach(key => deleteCookie(key)); // remove cookie
             navigate("/auth/login", { replace: true, state: { from: location } });
+            queryClient.clear();
             setAuth(null);
         }
     });
