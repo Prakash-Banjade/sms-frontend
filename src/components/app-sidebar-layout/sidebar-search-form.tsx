794db4ce32c360065f6593571f4ace8778b7bbd8
Prakash Banjade <prakashbanjade191@gmail.com>
2025-02-01 18:55:26 +0545

    feat: add shortcut btn to focus on sidebar search

--- src/components/app-sidebar-layout/sidebar-search-form.tsx
+++ src/components/app-sidebar-layout/sidebar-search-form.tsx
@@ -1,55 +1,77 @@
-import { Search } from "lucide-react"
+import { Command, Search } from "lucide-react"
 
 import { Label } from "@/components/ui/label"
 import {
+    SIDEBAR_SEARCH_KEYBOARD_SHORTCUT,
     SidebarGroup,
     SidebarGroupContent,
+    SidebarInput,
     useSidebar,
 } from "@/components/ui/sidebar"
-import { useRef, useState } from "react";
-import { Input } from "../ui/input";
+import { useEffect, useRef, useState } from "react";
 
 export function SidebarSearchForm() {
     const { search, setSearch } = useSidebar();
     const [inputVal, setInputVal] = useState(search);
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const onSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setSearch(inputVal?.trim());
+        }, 500);
 
-        const search = e.currentTarget.search.value;
+        return () => clearTimeout(handler);
+    }, [inputVal]);
 
-        setSearch(search || "");
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputVal(event.target.value);
+    };
+
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (
+                event.key === SIDEBAR_SEARCH_KEYBOARD_SHORTCUT &&
+                (event.metaKey || event.ctrlKey)
+            ) {
+                event.preventDefault()
+                inputRef.current?.focus()
+            }
+        }
+
+        window.addEventListener("keydown", handleKeyDown)
+        return () => window.removeEventListener("keydown", handleKeyDown)
+    }, [inputRef]);
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Escape") {
+            setSearch('');
+            setInputVal('');
+            inputRef.current?.blur()
+        }
     }
 
     return (
-        <form onSubmit={onSearchSubmit}>
-            <SidebarGroup className="p-0">
-                <SidebarGroupContent className="relative">
-                    <Label htmlFor="search" className="sr-only">
-                        Search
-                    </Label>
-                    <Input
-                        ref={inputRef}
-                        value={inputVal}
-                        onChange={(e) => setInputVal(prev => {
-                            const val = e.target.value;
-
-                            if (prev !== "" && val === "") { // auto reset if user clears input event if form is not submitted
-                                setSearch("");
-                            }
-
-                            return val;
-                        })}
-                        type="search"
-                        id="search"
-                        placeholder="Search menu..."
-                        className="pl-8"
-                    />
-                    <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
-                </SidebarGroupContent>
-            </SidebarGroup>
-        </form>
+        <SidebarGroup className="p-0">
+            <SidebarGroupContent className="relative">
+                <Label htmlFor="search" className="sr-only">
+                    Search
+                </Label>
+                <SidebarInput
+                    ref={inputRef}
+                    value={inputVal}
+                    onChange={handleInputChange}
+                    id="search"
+                    placeholder="Search menu..."
+                    className="peer pl-8"
+                    onKeyDown={handleKeyDown}
+                />
+                <div className="peer-focus-visible:hidden pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 select-none opacity-50 flex items-center gap-1 rounded-md px-1.5 py-0.5 bg-secondary border">
+                    <Command size={14} />
+                    <span className="text-sm capitalize">{SIDEBAR_SEARCH_KEYBOARD_SHORTCUT}</span>
+                </div>
+                <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+            </SidebarGroupContent>
+        </SidebarGroup>
     )
 }
 
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -24,6 +24,7 @@ const SIDEBAR_WIDTH = "18rem"
 const SIDEBAR_WIDTH_MOBILE = "18rem"
 const SIDEBAR_WIDTH_ICON = "3rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
+export const SIDEBAR_SEARCH_KEYBOARD_SHORTCUT = "k"
 const SIDEBAR_SEARCH_KEY = "s"
 
 type SidebarContext = {
