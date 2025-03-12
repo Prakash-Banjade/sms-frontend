34bfd713f6ae1a7caab91ae77d05ff0a3b09319d
Prakash Banjade <prakashbanjade191@gmail.com>
2025-03-12 15:44:22 +0545

    refactor: optimize page loading with lazy loading and improve loading skeleton

--- src/hooks/useLoadApperance.ts
+++ src/hooks/useLoadApperance.ts
@@ -1,15 +1,18 @@
 import { defaultThemeColor, THEME_COLOR_KEY, ThemeColor, themeColorSchema } from "@/apps/common/components/settings/appearance/appearance";
 import { defaultRadius, RADIUS_KEY, radiusSchema } from "@/apps/common/components/settings/appearance/radius-selector";
+import { useLayoutEffect } from "react";
 
 export function useLoadApperance() {
-    const { success: themeColorSuccess, data: themeColor } = themeColorSchema.safeParse(localStorage.getItem(THEME_COLOR_KEY) ?? defaultThemeColor);
-    const { success: radiusSuccess, data: radius } = radiusSchema.safeParse(localStorage.getItem(RADIUS_KEY) ?? defaultRadius);
+    useLayoutEffect(() => {
+        const { success: themeColorSuccess, data: themeColor } = themeColorSchema.safeParse(localStorage.getItem(THEME_COLOR_KEY) ?? defaultThemeColor);
+        const { success: radiusSuccess, data: radius } = radiusSchema.safeParse(localStorage.getItem(RADIUS_KEY) ?? defaultRadius);
 
-    if (themeColorSuccess) {
-        document.documentElement.classList.remove(...Object.values(ThemeColor));
-        document.documentElement.classList.add(themeColor);
-    }
-    if (radiusSuccess) {
-        document.documentElement.style.setProperty('--radius', radius);
-    }
+        if (themeColorSuccess) {
+            document.documentElement.classList.remove(...Object.values(ThemeColor));
+            document.documentElement.classList.add(themeColor);
+        }
+        if (radiusSuccess) {
+            document.documentElement.style.setProperty('--radius', radius);
+        }
+    }, []);
 }
