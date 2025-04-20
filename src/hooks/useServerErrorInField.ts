f6c8e3473289846783db93fd9221658efa158087
Prakash Banjade <prakashbanjade191@gmail.com>
2025-04-20 11:42:09 +0545

    fix: change undefined to any

--- src/hooks/useServerErrorInField.ts
+++ src/hooks/useServerErrorInField.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { UseFormReturn } from "react-hook-form";
 
-export const useServerErrorInField = (error: unknown, form: UseFormReturn<any, any, undefined>) => {
+export const useServerErrorInField = (error: unknown, form: UseFormReturn<any, any, any>) => {
     useEffect(() => { // show error directly in form field if send by server
         const errObj = (error as any)?.response?.data?.message;
         if (!!errObj?.field) {
